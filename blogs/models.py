88b2acd9795e17428eb8de2783848454c90c9f3e
Ehsan Doroudian <ehsan.dorodian@gmail.com>
2025-03-12 12:58:28 +0330

    create Blog model and BlogAdmin class

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from accounts.models import CustomUserModel
 
-# Create your models here.
+class Blog(models.Model):
+    STATUS_CHOICES = (
+        ('pub', 'publish'),
+        ('drf', 'draft'),
+    )
+    title = models.CharField(max_length=50)
+    user = models.ForeignKey(CustomUserModel, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    modfied_datetime = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=3)
+
