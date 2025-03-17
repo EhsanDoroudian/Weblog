aef6dc3a902aa5a2c7d4b8b44076a65395ce9451
Ehsan Doroudian <ehsan.dorodian@gmail.com>
2025-03-17 19:46:42 +0330

    create functional blog detail view

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', view=views.blog_list_view, name='blog_list'),
+    path('<int:pk>/', view=views.blog_detail_view, name='blog_detail')
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Blog
 
@@ -6,3 +6,8 @@ from .models import Blog
 def blog_list_view(request):
     blogs = Blog.objects.filter(status='pub')
     return render(request, 'blogs/blogs_list_page.html', context={'blogs': blogs})
+
+
+def blog_detail_view(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    return render(request, 'blogs/blog_detail_page.html', context={'blog': blog})
