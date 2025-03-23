28fefd826f722f583c7e956fd1caa3f202f0cf6a
EhsanDoroudian <ehsan14doroudian04@gmail.com>
2025-03-23 19:35:54 +0330

    create Comment Model

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import Blog
+from .models import Blog, Comment
 
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ['title', 'user', 'created_datetime', 'status']
     ordering = ('-modfied_datetime',)
+
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['text', 'user', 'blog', 'is_active']
+    ordering = ('-modfied_datetime',)
+
--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
 
-from .models import Blog
+from .models import Blog, Comment
 from .forms import BlogForm
 from .decorators import user_is_authorized
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
