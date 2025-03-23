625501c2c48603c4cde35b81da1d1ed7222b1055
EhsanDoroudian <ehsan14doroudian04@gmail.com>
2025-03-23 14:51:54 +0330

    add paginator in blog list view

--- blogs/views.py
+++ blogs/views.py
@@ -4,11 +4,23 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import Blog
 from .forms import BlogForm
 from .decorators import user_is_authorized
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def blog_list_view(request):
-    blogs = Blog.objects.filter(status='pub')
-    return render(request, 'blogs/blogs_list_page.html', context={'blogs': blogs})
+    blogs_list = Blog.objects.filter(status='pub')
+    paginator = Paginator(blogs_list, 7) 
 
+    page = request.GET.get('page')  # Get the current page number from the request
+    try:
+        blogs = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver the first page
+        blogs = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g., 9999), deliver the last page
+        blogs = paginator.page(paginator.num_pages)
+
+    return render(request, 'blogs/blogs_list_page.html', context={'blogs': blogs})
 
 @login_required
 def blog_detail_view(request, pk):
