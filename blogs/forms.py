877b12fb3eca30c944be3c2ca1a87f40c48415e1
EhsanDoroudian <ehsan14doroudian04@gmail.com>
2025-03-24 06:52:00 +0330

    implement comment form in blog detail page

--- blogs/forms.py
+++ blogs/forms.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm
 
-from .models import Blog
+from .models import Blog, Comment
 
 
 class BlogForm(ModelForm):
     class Meta:
         model = Blog
         fields = ['title', 'body', 'status']
+
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text', ]
