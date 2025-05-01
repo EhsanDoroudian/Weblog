a1c2f24faca795f1ba9513f1df1b5182ee9abd51
Ehsan Doroudian <ehsan14doroudian04@gmail.com>
2025-05-01 10:42:18 +0330

    add website url

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://ehsandrn22.pythonanywhere.com/']
 
 
 # Application definition
