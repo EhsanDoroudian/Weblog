6560794900983115fd0821ec514cd9fe4bd3beac
Ehsan Doroudian <ehsan14doroudian04@gmail.com>
2025-07-19 15:40:29 +0330

    improve Blog detail and Blog create tests

--- blogs/tests.py
+++ blogs/tests.py
@@ -151,6 +151,7 @@ class TestBlogList(TestBlog):
         self.assertEqual(blogs[0], self.blog1)  # Newer book first
         self.assertEqual(blogs[1], self.blog3)  # Older book second
 
+
 class TestBlogDetail(TestBlog):
 
     def test_blog_detail_url_redirects_unauthenticated_users(self):
@@ -159,26 +160,35 @@ class TestBlogDetail(TestBlog):
         self.assertRedirects(response, f'/accounts/login/?next=/blogs/{self.blog2.id}/')
     
     def test_blog_detail_url_name_redirects_unauthenticated_users(self):
-        url = reverse('blog_detail', args=[self.blog1.id])
+        url = reverse('blog_detail', kwargs={"pk": self.blog2.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, f'/accounts/login/?next={url}')
     
     def test_blog_detail_view_authenticated(self):
-        self.client.force_login(self.user1)  
-        response = self.client.get(reverse('blog_detail', args=[self.blog1.id]))
+        self.client.force_login(self.user1)
+        url = reverse('blog_detail', kwargs={"pk": self.blog1.id})  
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'blogs/blog_detail_page.html')
     
     def test_blog_detail_view_content(self):
         self.client.force_login(self.user1)
-        response = self.client.get(reverse('blog_detail', args=[self.blog1.id]))
+        url = reverse('blog_detail', kwargs={"pk": self.blog1.id})  
+        response = self.client.get(url)
         
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.blog1.body)
         self.assertContains(response, self.blog1.user)
+        self.assertContains(response, self.comment1)
         self.assertEqual(response.context['blog'], self.blog1)
         self.assertIsInstance(response.context['comment_form'], CommentForm)
+
+    def test_blog_detail_invalid_pk(self):
+        # Test with a non-existent blog ID
+        self.client.force_login(self.user1)
+        response = self.client.get(reverse('blog_detail', kwargs={'pk': 999}))
+        self.assertEqual(response.status_code, 404)
 
 
 class TestBlogCreate(TestBlog):
@@ -202,6 +212,11 @@ class TestBlogCreate(TestBlog):
         self.client.force_login(self.user1)
         response = self.client.get(reverse('blog_create'))
         self.assertEqual(response.status_code, 200)
+
+    def test_blog_view_authenticated_users_template_used(self):
+        self.client.force_login(self.user1)
+        response = self.client.get(reverse("blog_create"))
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'blogs/blog_create_page.html')
 
     def test_blog_view_form(self):
@@ -210,11 +225,10 @@ class TestBlogCreate(TestBlog):
         response = self.client.post(  
             path=url,
             data={
-                'title': 'title3', 
-                'body': 'body3',
+                'title': 'Title4', 
+                'body': 'Body4',
                 'status': 'drf',  
             },
-            follow=False  # Disable following redirects to check status 302
         )
         
         # Debug: Print form errors if status is not 302
@@ -225,12 +239,39 @@ class TestBlogCreate(TestBlog):
         blog = Blog.objects.last()
         self.assertIsNotNone(blog)  # Ensure blog was created
         self.assertEqual(Blog.objects.last().user, self.user1)
-        self.assertEqual(Blog.objects.last().title, 'title3')
-        self.assertEqual(Blog.objects.last().body, 'body3')
+        self.assertEqual(Blog.objects.last().title, 'Title4')
+        self.assertEqual(Blog.objects.last().body, 'Body4')
         self.assertEqual(Blog.objects.last().status, 'drf')
 
         self.assertEqual(response.url, blog.get_absolute_url())  # Check redirect URL
 
 
+
+    def test_blog_create_post_unauthenticated(self):
+        blog_data = {
+            'title': 'New Book',
+            'body': 'New Body',
+        }
+        response = self.client.post(reverse('blog_create'), data=blog_data)
+        self.assertEqual(response.status_code, 302)  # Redirect to login
+        self.assertRedirects(response, '/accounts/login/?next=/create/')
+        self.assertEqual(Blog.objects.all().count(), 3)  # No book created
+
+
 class TestBlogUpdate(TestBlog):
-    pass
+
+    def test_blog_update_url_auauthenticated(self):
+        response = self.client.get(reverse("blog_update", kwargs={"pk": self.blog1.id}))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f'/accounts/login/?next=/update/{self.blog1.id}/')
+
+    def test_blog_update_authenticated_owner(self):
+        self.client.force_login(self.user1)
+        response = self.client.get(reverse("blog_update", kwargs={"pk": self.blog1.id}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_book_update_template_used(self):
+        self.client.force_login(self.user1)
+        response = self.client.get(reverse("blog_update", kwargs={"pk": self.blog1.id}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "blogs/blog_update_page.html")
