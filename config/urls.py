82474d8305cf7b4fb9f5a5ba4ae533f572bd756f
Ehsan Doroudian <ehsan14doroudian04@gmail.com>
2025-07-27 11:43:42 +0330

    add debug toolbar to prject

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,15 @@
+import os
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.views.generic import TemplateView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 from django.conf import settings
 from django.conf.urls.static import static
+import sys
 
+# Detect if running tests
+TESTING = 'test' in sys.argv or 'PYTEST_VERSION' in os.environ
 
 # drf-yasg Schema View Configuration
 schema_view = get_schema_view(
@@ -34,17 +37,21 @@ urlpatterns = [
     path('', include('blogs.urls')),  # Default/homepage
     
     # API Documentation URLs
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', 
-            schema_view.without_ui(cache_timeout=0), 
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0),
             name='schema-json'),
-    path('swagger/', 
-         schema_view.with_ui('swagger', cache_timeout=0), 
+    path('swagger/',
+         schema_view.with_ui('swagger', cache_timeout=0),
          name='schema-swagger-ui'),
-    path('redoc/', 
-         schema_view.with_ui('redoc', cache_timeout=0), 
+    path('redoc/',
+         schema_view.with_ui('redoc', cache_timeout=0),
          name='schema-redoc'),
 ]
 
+# Add debug toolbar URLs only in DEBUG mode and not testing
+if settings.DEBUG and not TESTING:
+    urlpatterns = [path('__debug__/', include('debug_toolbar.urls'))] + urlpatterns
 
+# Serve static files in production
 if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
